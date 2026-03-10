f3a26a571f7835631c54e2808dc4e0c5a75c5d04
Husnain <husnainhaider1218@gmail.com>
2026-03-10 09:29:21 +0000

    Added initial import libraries and CSS file for Navbar component

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,4 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import "./Navbar.css";
