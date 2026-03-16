e3616e293f31ab4af1227334a7f3248211e449f7
Husnain <husnainhaider1218@gmail.com>
2026-03-16 09:32:16 +0000

    Added Libraries  to Navbar components in AppRoutes.jsx file

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "../components/layout/Navbar";
+import Footer from "../components/layout/Footer";
+import Home from "../pages/Home/Home";
