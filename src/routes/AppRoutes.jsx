7ddd8838fb4bae1010ab8311282d8b8cc9fc42a1
Husnain <husnainhaider1218@gmail.com>
2026-03-18 00:50:11 +0000

    Added import of ProtectedRoute component

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,3 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 import Home from "../pages/Home/Home";
+
+
+
+import ProtectedRoute from "../components/common/ProtectedRoute";
