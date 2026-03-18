6b64cf87d421f8fafe0c97ed4dd5c362427fe683
Husnain <husnainhaider1218@gmail.com>
2026-03-18 00:47:39 +0000

    Added  ProtectedRoute component

--- src/components/common/ProtectedRoute.jsx
+++ src/components/common/ProtectedRoute.jsx
@@ -2,3 +2,6 @@ import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import Loader from "./Loader";
+function ProtectedRoute({ children }) {
+
+}
