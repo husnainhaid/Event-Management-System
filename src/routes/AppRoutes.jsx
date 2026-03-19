0a733123c375b0c5253c5621cb5c5eaf80365833
Husnain <husnainhaider1218@gmail.com>
2026-03-19 12:19:04 +0000

    set up initial application structure including routing, authentication context, protected routes, and a navigation bar.

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,21 +3,19 @@ import { Routes, Route } from "react-router-dom";
 
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
+import ProtectedRoute from "../components/common/ProtectedRoute";
 import Home from "../pages/Home/Home";
+
 function AppRoutes() {
- return (
-      <>
-             <Navbar />
-        <Routes>
+  return (
+    <>
+      <Navbar />
+      <Routes>
         <Route path="/" element={<Home />} />
-
-
-        </Routes>
-      </>
-
- );
-
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
-
-import ProtectedRoute from "../components/common/ProtectedRoute";
+export default AppRoutes;
