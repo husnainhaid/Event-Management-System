4ed8d8af9f4367904ede76afd11c3c1dea698ef7
Husnain <husnainhaider1218@gmail.com>
2026-03-24 10:28:56 +0000

    Import pages there

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,8 +3,20 @@ import { Routes, Route } from "react-router-dom";
 
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
-import ProtectedRoute from "../components/common/ProtectedRoute";
+
 import Home from "../pages/Home/Home";
+import Events from "../pages/Events/Events";
+import EventDetails from "../pages/EventDetails/EventDetails";
+import CreateEvent from "../pages/CreateEvent/CreateEvent";
+import EditEvent from "../pages/EditEvent/EditEvent";
+import MyBookings from "../pages/MyBookings/MyBookings";
+import AdminDashboard from "../pages/AdminDashboard/AdminDashboard";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import NotFound from "../pages/NotFound/NotFound";
+
+import ProtectedRoute from "../components/common/ProtectedRoute";
+import AdminRoute from "../components/common/AdminRoute"; 
 
 function AppRoutes() {
   return (
