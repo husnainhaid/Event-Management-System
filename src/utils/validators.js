1733333d25b31c0f25e9004d4b6563bac91931a6
Husnain <husnainhaider1218@gmail.com>
2026-03-24 10:09:05 +0000

    `Added email and password validation functions`

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,11 +1,14 @@
-// validators.js - Form and data validation helper functions
+export function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email) return "Email is required";
+    if (!re.test(email)) return "Enter a valid email address";
+    return null;
+}
 
-export const isValidEmail = (email) => {
-    // TODO: implement
-    return true;
-};
-
-export const isRequired = (value) => {
-    // TODO: implement
-    return true;
-};
+export function validatePassword(password) {
+    if (!password) return "Password is required";
+    if (password.length < 8) return "Password must be at least 8 characters";
+    if (!/[A-Z]/.test(password)) return "Include at least one uppercase letter";
+    if (!/[0-9]/.test(password)) return "Include at least one number";
+    return null;
+}
