f1e93aa3a1d36d5ce4061f8c0540025434718116
Husnain <husnainhaider1218@gmail.com>
2026-04-01 22:57:02 +0100

    adding attributes

--- src/pages/EditEvent/EditEvent.jsx
+++ src/pages/EditEvent/EditEvent.jsx
@@ -5,3 +5,17 @@ import { getEventById, updateEvent } from "../../services/eventService";
 import EventForm from "../../components/events/EventForm";
 import Loader from "../../components/common/Loader";
 import Alert from "../../components/common/Alert";
+
+function EditEvent() {
+
+ const { id } = useParams();
+    const { user } = useAuth();
+    const navigate = useNavigate();
+
+    const [event, setEvent] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [saving, setSaving] = useState(false);
+    const [error, setError] = useState("");
+
+}
+export default EditEvent;
