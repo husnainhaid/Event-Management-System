59960efa8cc145d8b989cc35fb05295af626d675
Husnain <husnainhaider1218@gmail.com>
2026-04-02 13:34:38 +0100

    Added new Field component and default event form data

--- src/components/events/EventForm.jsx
+++ src/components/events/EventForm.jsx
@@ -2,3 +2,40 @@ import React, { useState } from "react";
 import Alert from "../common/Alert";
 import { CATEGORIES } from "../../utils/constants";
 import { validateEventForm } from "../../utils/validators";
+
+const EMPTY_FORM = {
+    title: "",
+    description: "",
+    category: "",
+    date: "",
+    time: "",
+    location: "",
+    city: "",
+    country: "Ireland",
+    price: 0,
+    capacity: 50,
+    image: "",
+    tags: "",
+};
+
+
+function Field({ label, name, type = "text", placeholder = "", min, step, form, errors, onChange }) {
+    return (
+        <div className="form-group">
+            <label htmlFor={name}>{label}</label>
+            <input
+                id={name}
+                name={name}
+                type={type}
+                value={form[name]}
+                onChange={onChange}
+                placeholder={placeholder}
+                min={min}
+                step={step}
+                aria-invalid={!!errors[name]}
+            />
+            {errors[name] && <span className="form-error">{errors[name]}</span>}
+        </div>
+    );
+}
+
